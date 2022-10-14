696d5eeb91c88599645f42d5b45925c79ec7896a
Kassidy White <kassidywhite514@gmail.com>
2022-10-14 23:21:16 +0000

    added script to html

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("myBtn").addEventListener("click", function(event)) {
     event.preventDefault();
     //const element = document.getElementById("getquotes");
@@ -10,12 +11,28 @@ document.getElementById("myBtn").addEventListener("click", function(event)) {
         .then(function(response)) {
             console.log(response);
             return response.json();
-        }).then(function(json) {
+        }).then(function(json)){
     console.log(json);
-    let page = "<p>";
-    page += json.quote[];
+    let page = "";
+    page += "<p>" + json["quote"];
     page += "</p>";
     document.getElementById("getquotes").innerHTML = page;
 });
 //}
 }
+*/
+/*
+document.getElementById("myBtn").addEventListener("click", function() {
+    console.log("hi");
+});
+    
+    const myurl = "https://api.kanye.rest";
+    const response = fetch(myurl)
+    */
+//const btn = document.getElementById('myBtn');
+/*
+btn.addEventListener('click', () => {
+    console.log('btn clicked');
+  });
+  */
+ //console.log(btn);
